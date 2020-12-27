c695040d03edce854ae77c68ad3fd96edd9abf8d
Lucas Hiago <lucashiago63@gmail.com>
2020-12-27 01:23:07 -0300

    feat: breakpoint

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import http.server
 
+from database import DBClient
 from handler import CustomHandler
 
 PORT = 8000
@@ -7,4 +8,9 @@ HOST = "0.0.0.0"
 Handler = CustomHandler
 
 with http.server.ThreadingHTTPServer((HOST, PORT), Handler) as httpd:
+    print(f"Running Server on port {PORT}")
+    print("Creating Database")
+    client = DBClient()
+    client.create_table()
+    print("Serving...")
     httpd.serve_forever()
