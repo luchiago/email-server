e479071d1d9b6a53e4c63638462c129753dac1f6
Lucas Hiago <lucashiago63@gmail.com>
2020-12-28 10:42:49 -0400

    feat: post requests finished

--- handler.py
+++ handler.py
@@ -1,53 +1,13 @@
 import http.server
-from os import name
 import re
 from json import dumps
-from typing import Optional
 
-from database import DBClient
-from status import (
-    HTTP_200_OK,
-    HTTP_204_NO_CONTENT,
-    HTTP_401_UNAUTHORIZED,
-    HTTP_404_NOT_FOUND,
-)
+from database import find_user, transaction_operation
+from formatters import format_user
+from status import HTTP_200_OK, HTTP_201_CREATED, HTTP_404_NOT_FOUND
 
-NOT_FOUND_RESOURCE = "Not found"
-NOT_FOUND_URL = "This URL does not exists"
-
-client = DBClient()
-
-
-def get_reply(messageid: Optional[int]):
-    if messageid is not None:
-        reply = (
-            client.search_one("SELECT * FROM message WHERE messageid = ?", messageid),
-        )
-        return parse_message(reply)
-    return None
-
-
-def parse_message(result: tuple):
-    reply = get_reply(result[5])
-    return {
-        "messageid": result[0],
-        "sender": client.search_one(
-            "SELECT name FROM user WHERE userid = ?", result[1]
-        ),
-        "receiver": client.search_one(
-            "SELECT name FROM user WHERE userid = ?", result[2]
-        ),
-        "subject": result[3],
-        "body": result[4],
-        "reply": reply,
-    }
-
-
-def parse_messages(result: list):
-    parsed = []
-    for message in result:
-        parsed.append(parse_message(message))
-    return parsed
+NOT_FOUND_RESOURCE = "não encontrado"
+NOT_FOUND_URL = "Esta URL não existe"
 
 
 class CustomHandler(http.server.BaseHTTPRequestHandler):
@@ -55,99 +15,50 @@ class CustomHandler(http.server.BaseHTTPRequestHandler):
         response = {"error": error_message}
         self.prepare_response(response=response, status=HTTP_404_NOT_FOUND)
 
-    def prepare_response(self, response: dict, status: int):
+    def prepare_response(self, response: dict, status: int = HTTP_200_OK):
         response = dumps(response).encode(encoding="utf_8")
         self.send_response(status)
         self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write(response)
 
-    def _find_a_message(self):
-        message_id = int(re.search("^/messages/(?P<id>\d+)/?$", self.path).gropus()[0])
-        return (
-            client.search_one(table_name="message", value=message_id, condition="id"),
-            message_id,
-        )
-    
-    def _get_body_request(self):
-        content_length = int(self.headers['Content-Length'])
+    def get_body_request(self):
+        content_length = int(self.headers["Content-Length"])
         post_data = self.rfile.read(content_length)
-        return eval(post_data.decode('utf-8'))
+        return eval(post_data.decode("utf-8"))
 
-
-    def _get_user_from_headers(self):
+    def get_user_from_headers(self):
         headers = dict(self.headers)
         return headers.get("user_id")
 
-    def do_GET(self):
-        if re.search("^/messages/?$", self.path) is not None:
-            user_id = self._get_user_from_headers()
-            if user_id is None:
-                self.prepare_response({"error: Não autorizado"}, HTTP_401_UNAUTHORIZED)
-                return
-            as_sender = client.search_all(
-                table_name="message", value=user_id, condition="sender"
-            )
-            as_receiver = client.search_all(
-                table_name="message", value=user_id, condition="receiver"
-            )
-            as_sender = parse_messages(as_sender)
-            as_receiver = parse_messages(as_receiver)
-            messages = {"sent": as_sender, "received": as_receiver}
-            self.prepare_response(messages, HTTP_200_OK)
-        elif re.search("^/messages/(?P<id>\d+)/?$", self.path) is not None:
-            message, _ = self._find_a_message()
-            if message is None:
-                self.not_found(NOT_FOUND_RESOURCE)
-                return
-            message = parse_message(message)
-            self.prepare_response(message, HTTP_200_OK)
-        else:
-            self.not_found(NOT_FOUND_URL)
-        return
-
     def do_POST(self):
         if re.search("^/messages/?$", self.path) is not None:
-            pass
+            message_data = self.get_body_request()
+            sender = find_user(id=self.get_user_from_headers())[0]
+            receiver = find_user(name=message_data.get("receiver"))[0]
+            if sender is None or receiver is None:
+                self.not_found(f"Usuário {NOT_FOUND_RESOURCE}")
+                return
+            command = "INSERT INTO message (sender, receiver, subject, body) VALUES (?, ?, ?, ?)"
+            values = (
+                sender,
+                receiver,
+                message_data.get("subject"),
+                message_data.get("body"),
+            )
+            transaction_operation(command=command, values=values)
+            self.prepare_response(
+                response={"message": "Email enviado"}, status=HTTP_201_CREATED
+            )
+            return
         elif re.search("^/login/?$", self.path) is not None:
-            data = self._get_body_request()
-            name = data.get('name')
-            user = client.find_user(name)
-        else:
-            self.not_found(NOT_FOUND_URL)
-        return
-
-    def do_PATCH(self):
-        if re.search("^/messages/(?P<id>\d+)/?$", self.path) is not None:
-            message, message_id = self._find_a_message()
-            if message is None:
-                self.not_found(NOT_FOUND_RESOURCE)
+            name = self.get_body_request().get("name")
+            user = find_user(name)
+            if user is None:
+                self.not_found(f"Usuário {NOT_FOUND_RESOURCE}")
                 return
-        else:
-            self.not_found(NOT_FOUND_URL)
-        return
-
-    def do_PUT(self):
-        if re.search("^/messages/(?P<id>\d+)/?$", self.path) is not None:
-            message, message_id = self._find_a_message()
-            if message is None:
-                self.not_found(NOT_FOUND_RESOURCE)
-                return
-        else:
-            self.not_found(NOT_FOUND_URL)
-        return
-
-    def do_DELETE(self):
-        if re.search("^/messages/(?P<id>\d+)/?$", self.path) is not None:
-            message, message_id = self._find_a_message()
-            if message is None:
-                self.not_found(NOT_FOUND_RESOURCE)
-                return
-            command = "UPDATE message SET receiver = NULL WHERE id = ? AND receiver = ?"
-            client.transaction_operation(command, (message_id, user_id))
-            command = "UPDATE message SET sender = NULL WHERE id = ? AND sender = ?"
-            client.transaction_operation(command, (message_id, user_id))
-            self.send_response(HTTP_204_NO_CONTENT)
+            json = format_user(user)
+            self.prepare_response(json)
         else:
             self.not_found(NOT_FOUND_URL)
         return
