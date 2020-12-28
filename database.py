23691ec7de8a44b60da3c4ce31cf6d5abbb7bdb5
Lucas Hiago <lucashiago63@gmail.com>
2020-12-28 15:23:51 -0400

    fix: corrects database problems

--- database.py
+++ database.py
@@ -1,17 +1,21 @@
 import sqlite3
 
-DB_NAME = "email-sever.db"
+DB_NAME = "email-server.db"
+
 
 def connect_to_database():
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
     return conn, cursor
 
+
 def transaction_operation(command: str, values: tuple):
     conn, cursor = connect_to_database()
     cursor.execute(command, values)
     conn.commit()
     conn.close()
+    return cursor.lastrowid
+
 
 def search_in_database(command: str, values: tuple, one: bool = True):
     conn, cursor = connect_to_database()
@@ -19,6 +23,7 @@ def search_in_database(command: str, values: tuple, one: bool = True):
     result = cursor.fetchone() if one else cursor.fetchall()
     conn.close()
     return result
+
 
 def find_user(name: str = None, id: int = -1):
     sql = "SELECT * FROM user WHERE"
@@ -28,13 +33,13 @@ def find_user(name: str = None, id: int = -1):
     sql += " id = ?"
     return search_in_database(command=sql, values=(id,))
 
+
 def find_message(id: int = -1, receiver: int = -1, sender: int = -1):
-    sql = (
-        "SELECT DISTINCT * FROM message WHERE id = ? OR receiver = ? OR sender = ?"
-    )
+    sql = "SELECT DISTINCT * FROM message WHERE id = ? OR receiver = ? OR sender = ?"
     return search_in_database(
         command=sql, values=(id, receiver, sender), one=(id != -1)
     )
+
 
 def create_table():
     conn, cursor = connect_to_database()
